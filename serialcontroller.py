f993ed208480327b1ef9c6d252d3cc7a86eea89f
Niles Alexis <pl232602@ahschool.com>
2023-12-28 15:05:03 -0500

    threading implementation

--- serialcontroller.py
+++ serialcontroller.py
@@ -1,47 +1,43 @@
 import serial
 import time
-import multiprocessing as mp
-import os
-import signal
-
+import threading as th
 
 previous_run = 0
 clean = 0
-current = "none"
+current_thread = None
+stop_event = th.Event()  # Event to signal the thread to stop
 
 def straight(speed):
     global clean
     compass_vals = []
 
-    microbit = serial.Serial("COM5",115200,timeout = 0.1)
+    microbit = serial.Serial("COM5", 115200, timeout=0.1)
     x = 0
 
-    ##Gets four compass readings and avergaes for accurate initial
-    ##direction
-    
-    
+    # Gets four compass readings and averages for accurate initial direction
     while x < 5:
         microbit.write(b"mv000000\n")
         data = microbit.readline().decode('utf-8').rstrip()
-        if len(data)==3:
+        if len(data) == 3:
             clean = data
             compass_vals.append(int(clean))
-        x=x+1
-        
-    init_direction = sum(compass_vals)/len(compass_vals)
+        x = x + 1
 
-
+    init_direction = sum(compass_vals) / len(compass_vals)
 
     speed_left = speed
     speed_right = speed
     base_speed = speed
-    
-    while True:
-        motor_speeds = f"mv{speed_left}{speed_right}\n" 
+
+    while not stop_event.is_set():
+        motor_speeds = f"mv{speed_left}{speed_right}\n"
         microbit.write(motor_speeds.encode("utf-8"))
         data = microbit.readline().decode('utf-8').rstrip()
-        if len(data)==3:
+        if len(data) == 3:
             clean = data
+
+    # Ensure that the microbit serial connection is properly closed
+    microbit.close()
 
 def left():
     pass
@@ -49,17 +45,52 @@ def left():
 def right():
     pass
 
-def motor_controller(speed,direction):
-    global current
-    if current != "none":
-        os.kill(current, signal.SIGTERM)
-    if direction == "straight":
-        motorp = mp.Process(target = straight, args=(speed,))
-    elif direction == "right":
-        motorp = mp.Process(target = right, args=(speed,))
-    elif direction == "left":
-        motorp = mp.Process(target = left, args=(speed,))
-    motorp.start()
-    current = motorp.pid
-    print(current)
+class MotorThread(th.Thread):
+    def __init__(self, speed, direction):
+        super().__init__()
+        self.speed = speed
+        self.direction = direction
 
+    def run(self):
+        global current_thread
+        if current_thread and current_thread.is_alive():
+            stop_event.set()  # Signal the current thread to stop
+            current_thread.join()
+
+        stop_event.clear()  # Reset the stop event for the new thread
+
+        if self.direction == "straight":
+            current_thread = self
+            self.straight()
+        elif self.direction == "right":
+            current_thread = self
+            self.right()
+        elif self.direction == "left":
+            current_thread = self
+            self.left()
+
+    def straight(self):
+        straight(self.speed)
+
+    def left(self):
+        left()
+
+    def right(self):
+        right()
+
+# Usage:
+# Call motor_controller to start/stop the motor movement
+# Example: motor_controller(50, "straight")
+# Example: motor_controller(30, "left")
+# Example: motor_controller(40, "right")
+def motor_controller(speed, direction):
+    global current_thread
+    if current_thread and current_thread.is_alive():
+        stop_event.set()  # Signal the current thread to stop
+        current_thread.join()
+
+    stop_event.clear()  # Reset the stop event for the new thread
+
+    motor_thread = MotorThread(speed, direction)
+    motor_thread.start()
+    current_thread = motor_thread
