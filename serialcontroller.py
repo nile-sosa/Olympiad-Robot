ddf365be3adf8239a63576a3229b926c4b8140ab
Niles Alexis <90462248+pl232602@users.noreply.github.com>
2024-01-02 11:49:03 -0500

    Update serialcontroller.py

--- serialcontroller.py
+++ serialcontroller.py
@@ -2,31 +2,14 @@ import serial
 import threading as th
 
 
-clean = 0
 current_thread = None
 stop_event = th.Event()
 
 
 def straight(speed):
-    global clean
-    compass_vals = []
 
     microbit = serial.Serial("COM5",115200,timeout = 0.1)
-    x = 0
 
-    ##Gets four compass readings and avergaes for accurate initial
-    ##direction
-    
-    
-    while x < 5:
-        microbit.write(b"mv000000\n")
-        data = microbit.readline().decode('utf-8').rstrip()
-        if len(data)==3:
-            clean = data
-            compass_vals.append(int(clean))
-        x=x+1
-        
-    init_direction = sum(compass_vals)/len(compass_vals)
 
     speed_left = speed
     speed_right = speed
@@ -37,9 +20,7 @@ def straight(speed):
             motor_speeds = f"mv{speed_left}{speed_right}\n" 
             microbit.write(motor_speeds.encode("utf-8"))
             data = microbit.readline().decode('utf-8').rstrip()
-            if len(data)==3:
-                clean = data
-            print(clean)
+            print(data)
     except KeyboardInterrupt as a:
         pass
 
