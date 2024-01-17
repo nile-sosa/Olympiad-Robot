65981ed9cab4f7ca0e750a1aa99120319b3d4a0b
Niles Alexis <pl232602@ahschool.com>
2024-01-17 04:59:35 +0000

    tested encoders, they are no good

--- serialtest.py
+++ serialtest.py
@@ -5,13 +5,14 @@ enc2 = Encoder.Encoder(23,24)
 microbit = serial.Serial("/dev/ttyACM0",115200,timeout = 0.001)
 x=0
 clean = 0
-while x<2050:
-    if enc1.read()<enc2.read():
-        microbit.write(b'mv050030\n')
-    elif enc2.read()<enc1.read():
-        microbit.write(b'mv030050\n')
-    else:
-        microbit.write(b'mv050050\n')
+while x<2500:
+    ##if (enc1.read()*0.9)<enc2.read():
+    ##    microbit.write(b'mv050040\n')
+    ##elif enc2.read()<(enc1.read()*0.9):
+    ##    microbit.write(b'mv045045\n')
+    ##else:
+    ##    microbit.write(b'mv050045\n')
+    microbit.write(b'mv030030\n')
     data = microbit.readline().decode('utf-8').rstrip()
     if len(data)==3:
         clean = data
