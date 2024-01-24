54d21d7f15b971c75c63184943c42321dbc0f32c
Niles Alexis <90462248+pl232602@users.noreply.github.com>
2024-01-24 07:45:27 -0500

    converted to micropython

--- microbit.py
+++ microbit.py
@@ -1,21 +1,34 @@
-right = ""
-left = ""
-input2 = ""
-serial.set_baud_rate(BaudRate.BAUD_RATE115200)
+from microbit import *
 
-def on_forever():
-    global input2, left, right
-    input2 = serial.read_line()
-    left = input2.substr(2, 3)
-    right = input2.substr(5, 3)
-    if len(input2) == 8 and input2.substr(0, 2) == "mv":
-        if left.substr(0, 1) == "-":
-            motion.drive_wheel(Motor.LEFT, -100 - parse_float(left))
-        else:
-            motion.drive_wheel(Motor.LEFT, parse_float(left))
-        if right.substr(0, 1) == "-":
-            motion.drive_wheel(Motor.RIGHT, -100 - parse_float(right))
-        else:
-            motion.drive_wheel(Motor.RIGHT, parse_float(right))
-    serial.write_line(input2)
-basic.forever(on_forever)
+uart.init(baudrate=115200)
+
+right_motor = pin13
+reverse_right = pin14
+left_motor = pin15
+reverse_left = pin16
+
+while True:
+    sleep(10)
+    if uart.any():
+        serial_input = uart.readline()
+        if serial_input is not None:
+            input2 = serial_input.decode('utf-8')# Decode the bytes to string
+            left = input2[2:5]  # Use slicing instead of substr
+            right = input2[5:8]  # Use slicing instead of substr
+            print('recieved')
+            print(input2)
+            print(right)
+            if len(input2) == 8 and input2[:1] == "mv":
+                if left[0] == "-":
+                    left_motor.write_analog(int(left[1:]))
+                    reverse_left.write_digital(1)
+                else:
+                    left_motor.write_analog(int(left))
+                    reverse_left.write_digital(0)  # Correct the pin to reverse_left
+                if right[0] == "-":
+                    right_motor.write_analog(int(right[1:]))
+                    reverse_right.write_digital(1)  # Correct the pin to reverse_right
+                else:
+                    right_motor.write_analog(int(right))
+                    reverse_right.write_digital(0)
+
