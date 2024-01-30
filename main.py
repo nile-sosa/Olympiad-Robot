76ce7cfd942ae97ce046d5cafc6036cc71545740
Niles Alexis <pl232602@ahschool.com>
2024-01-30 23:22:29 +0000

    finished up right and reverse movement commands, just need to add button start and tuning

--- main.py
+++ main.py
@@ -6,12 +6,4 @@ import sys
 
 ##since daemon threading is used, time wait must be used to keep motor program running
 if __name__ == "__main__":
-    motor_controller(None,"straight",500)
-    motor_controller(None,"left",0)
-    motor_controller(None,"straight",200)
-    motor_controller(None,"left",0)
-    motor_controller(None,"straight",500)
-    motor_controller(None,"left",0)
-    motor_controller(None,"straight",200)
-    motor_controller(None,"left",0)
-
+    motor_controller(None,"straight",1000)
--- serialcontroller.py
+++ serialcontroller.py
@@ -14,14 +14,14 @@ stop_event = th.Event()
 manager = Manager()
 encoder_values = manager.list([0,0])
 incrementor_list = [0]
-
+desired_encoder_position = [0,0]
 
 
 def straight(distance):
     global encoder_values
-    kp = 4.0
-    ki = 2
-    kd = 1
+    kp = 6.5
+    ki = 1.8
+    kd = 1.3
     left_pid_motor = PID(kp,ki,kd,setpoint = 0)
     right_pid_motor = PID(kp,ki,kd,setpoint = 0)
     init_left = encoder_values[0]
@@ -45,9 +45,12 @@ def straight(distance):
             print(right_pid_output)
             speed_left = int(left_pid_output)
             speed_right = int(right_pid_output)
-
-            motor_speeds = f"mv0{speed_left}0{speed_right}\n"   
-            microbit.write(motor_speeds.encode("utf-8"))
+            if speed_left>=10:
+                motor_speeds = f"mv0{speed_left}0{speed_right}\n" 
+                microbit.write(motor_speeds.encode("utf-8"))
+            elif speed_left<=-10:
+                motor_speeds = f"mv{speed_left}{speed_right}\n" 
+                microbit.write(motor_speeds.encode("utf-8"))
             data = microbit.readline().decode('utf-8').rstrip()
             print([left_enc_val,right_enc_val])
             #print(motor_speeds)
@@ -64,7 +67,54 @@ def straight(distance):
 
     microbit.close() 
 
+def reverse(distance):
+    global encoder_values
+    kp = 4.0
+    ki = 1.4
+    kd = 1.5
+    left_pid_motor = PID(kp,ki,kd,setpoint = 0)
+    right_pid_motor = PID(kp,ki,kd,setpoint = 0)
+    init_left = encoder_values[0]
+    init_right = encoder_values[1]
+    left_pid_motor.output_limits=(-900,900)
+    right_pid_motor.output_limits=(-900,900)
+    print("starting motors forward")
+    i = 0
+    microbit = serial.Serial("/dev/ttyACM0",115200,timeout = 0)
+    try:
+        while encoder_values[0]-init_left >= distance:
+            time.sleep(0.05)
+            left_pid_motor.setpoint = incrementor_list[0]
+            right_pid_motor.setpoint = incrementor_list[0]
+            left_enc_val = encoder_values[0] - init_left
+            right_enc_val = encoder_values[1] - init_right
+
+            left_pid_output = left_pid_motor(left_enc_val)/10
+            right_pid_output = right_pid_motor(right_enc_val)/10
+            print(left_pid_output)
+            print(right_pid_output)
+            speed_left = int(left_pid_output)
+            speed_right = int(right_pid_output)
+            motor_speeds = f"mv{speed_left}{speed_right}\n"
+            microbit.write(motor_speeds.encode("utf-8"))
+            data = microbit.readline().decode('utf-8').rstrip()
+            print([left_enc_val,right_enc_val])
+            #print(motor_speeds)
+            print(incrementor_list)
+        while i<10:
+            motor_speeds = f"mv-00-00\n"
+            microbit.write(motor_speeds.encode("utf-8"))
+            data = microbit.readline().decode('utf-8').rstrip()
+            print("stopping")
+            i=i+1
+    except KeyboardInterrupt as a:
+        microbit.close()
+        pass
+    microbit.close()
+
+
 def left():
+
     global encoder_values
     kp = 4.5
     ki = 3
@@ -76,7 +126,7 @@ def left():
     init_right = encoder_values[1]
     print("turning left")
     i=0
-    turn = 147
+    turn = 148
     right_pid_motor.setpoint = -turn
     right_pid_motor.output_limits = (-650,650)
     left_pid_motor.setpoint = turn
@@ -115,7 +165,54 @@ def left():
 
 
 def right():
-    pass
+    global encoder_values
+    kp = 4.5 
+    ki = 3 
+    kd = 1.5 
+    left_pid_motor = PID(kp,ki,kd,setpoint = 0)
+    right_pid_motor = PID(kp,ki,kd,setpoint = 0)
+
+    init_left = encoder_values[0]
+    init_right = encoder_values[1]
+    print("turning right")
+    i=0 
+    turn = 150
+    right_pid_motor.setpoint = turn
+    right_pid_motor.output_limits = (-650,650)
+    left_pid_motor.setpoint = -turn
+    left_pid_motor.output_limits = (-650,650)
+    microbit = serial.Serial("/dev/ttyACM0",115200,timeout = 0)
+    try:
+        while encoder_values[1]-init_right<turn or encoder_values[0]-init_left>-turn:
+            time.sleep(0.05)
+            left_enc_val = encoder_values[0] - init_left
+            right_enc_val = encoder_values[1] - init_right
+
+            left_pid_output = left_pid_motor(left_enc_val)/10
+            right_pid_output = right_pid_motor(right_enc_val)/10
+            print(left_pid_output)
+            print(right_pid_output)
+            speed_left = int(left_pid_output)
+            speed_right = int(right_pid_output)
+            if encoder_values[1]-init_right>=turn:
+                speed_right = "00"
+            if encoder_values[0]-init_left<=-turn:
+                speed_left = "000"
+            motor_speeds = f"mv{speed_left}0{speed_right}\n"   
+            print(motor_speeds)
+            microbit.write(motor_speeds.encode("utf-8"))
+            data = microbit.readline().decode('utf-8').rstrip()
+            print([left_enc_val,right_enc_val])
+        while i<10:
+            motor_speeds = f"mv-00-00\n"   
+            microbit.write(motor_speeds.encode("utf-8"))
+            data = microbit.readline().decode('utf-8').rstrip()
+            print("stopping")
+            i=i+1
+    except KeyboardInterrupt as a:
+        microbit.close()
+        pass
+
 
 def motor_controller(speed,direction,distance):
     global bearing
@@ -133,24 +230,43 @@ def motor_controller(speed,direction,distance):
     if current_thread and current_thread.is_alive():
         stop_event.set()
         current_thread.join()
-
     stop_event.clear()
 
     if direction == "straight":
         motorp = th.Thread(target = straight, args=(distance,))
         current_thread = motorp
+        incrementor_list[0] = 0
+        incrementor_thread = th.Thread(target = incrementor, args=(incrementor_list,distance))
+        incrementor_thread.daemon = True
+        incrementor_thread.start()
+
+    elif direction == "reverse":
+        motorp = th.Thread(target = reverse, args=(distance,))
+        current_thread = motorp
+        incrementor_list[0] = 0
+        incrementor_thread = th.Thread(target = reverse_incrementor, args=(incrementor_list,distance))
+        incrementor_thread.daemon = True
+        incrementor_thread.start()
     elif direction == "left":
         motorp = th.Thread(target = left, args=())
         current_thread = motorp
-    incrementor_list[0] = 0
-    incrementor_thread = th.Thread(target = incrementor, args=(incrementor_list,distance))
-    incrementor_thread.daemon = True
-    incrementor_thread.start()
+    elif direction == "right":
+        motorp = th.Thread(target = right, args=())
+        current_thread = motorp
     current_thread.start()
+    incrementor_thread.join()
+
 
 def incrementor(incrementation_value,distance):
     while incrementation_value[0]<distance:
-        time.sleep(0.01)
+        time.sleep(0.015)
         incrementation_value[0] = incrementation_value[0] + 1
     print("incrementation stopped")
+
+def reverse_incrementor(incrementation_value,distance):
+    while incrementation_value[0]>distance:
+        time.sleep(0.01)
+        incrementation_value[0] = incrementation_value[0] - 1
+    print("reverse incrementation stopped")
+
 ##Setting daemon to true allows thread to end when main process finishes
